21573febf96343c22eea2214f1e81a70f1e303ad
ClaraBzk <38009877+ClaraBzk@users.noreply.github.com>
2019-10-26 22:39:38 +0200

    update

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -6,15 +6,19 @@ const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
-
+// Url 16 days
+const API_16 = "https://api.openweathermap.org/data/2.5/forecast/daily";
 
 class API_WEATHER{
   constructor(city){
     // Si la ville n'est pas définit alors la ville par défault est Paris
+
     if(city === undefined){
       city = "paris";
     }
     this.city = city;
+
+
   }
 
   // Faire la requete à l'API openweathermap
@@ -25,6 +29,17 @@ class API_WEATHER{
       crossdomain: true
     })
   }
+
+
+  getThreeDayForecast(){
+    
+    return axios
+    .get(`${API_16}?q=${this.city}&units=metric&appid=${API_KEY}&cnt=3`, {
+      crossdomain: true
+    })
+  }
+
+
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 
 // Fonction appelée lors du click du bouton
 function start() {
+  city = document.getElementById('city-input').value;
+  if(city === ""){ city=undefined;}
+
   // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
+  const apiWeather = new API_WEATHER(city);
+
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
@@ -28,4 +32,28 @@ function start() {
       // Affiche une erreur
       console.error(error);
     });
+
+
+
+
+
+      apiWeather.getThreeDayForecast().then(function(response) {
+        const data2 = response.data;
+  
+        // On récupère l'information principale
+        let jour = 0;
+        data2.list.forEach((data2, index) => {
+          jour++;
+          document.getElementById(`${jour}-forecast-main`).innerHTML = data2.weather[0].main;
+          document.getElementById(`${jour}-forecast-more-info`).innerHTML = data2.weather[0].description;
+          document.getElementById(`${jour}-icon-weather-container`).innerHTML = apiWeather.getHTMLElementFromIcon(data2.weather[0].icon);
+          document.getElementById(`${jour}-forecast-temp`).innerHTML = `${data2.temp.day}°C`;
+  
+        });
+
+  
+      });
+      
+  
 }
+
